d6a31d59f85e679a03ea84e366caca79caa6025f
unknown <bidaybek96@mail.ru>
2022-09-12 20:03:23 +0600

    lab1

--- lab1/a.cpp
+++ lab1/a.cpp
@@ -1,12 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> Cards(int a){
-
+void print(deque<int> dq){
+    while(!dq.empty()){
+        cout << dq.front() << ' ';
+        dq.pop_front();
+    }
+    cout << endl;
+}
+void Cards(int a, deque<int> &dq){
+    while(a > 0){
+        dq.push_front(a);
+        if(dq.size() > 1){
+            for(int i = 0; i < a; i++){
+                int tmp = dq.back();
+                dq.pop_back();
+                dq.push_front(tmp);
+            }
+        }
+        a--;
+    }
+    print(dq);
 }
 int main() {
-    vector<vector<int> > v;
     int n;
     cin >> n;
-    
+    while(n--){
+        deque<int> dq;  
+        int a;
+        cin >> a;
+        Cards(a, dq);
+    }
     return 0;
 }
